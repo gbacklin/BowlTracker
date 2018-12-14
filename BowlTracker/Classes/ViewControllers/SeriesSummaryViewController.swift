243856a597043e4db8802029ab13e14b92e64165
Gene Backlin <gbacklin@marizack.com>
2018-12-13 18:00:40 -0600

    Added ability to send the Series Summary as a mail attachment or as a Text Message attachment.

--- BowlTracker/Classes/ViewControllers/SeriesSummaryViewController.swift
+++ BowlTracker/Classes/ViewControllers/SeriesSummaryViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import MessageUI
 
 class SeriesSummaryViewController: UIViewController {
     
@@ -18,13 +19,117 @@ class SeriesSummaryViewController: UIViewController {
     var textTitle: String?
     var series: [[Frame]]?
 
+    // MARK: - View lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "Series Summary"
-        
         seriesSummaryLabel.text = textTitle
+        
+        let sendButton = UIBarButtonItem(title: "Send", style: .plain, target: self, action: #selector(SeriesSummaryViewController.send(_:)))
+        navigationItem.rightBarButtonItem = sendButton
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        title = "Series Summary"
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        textTitle = title
+        title = " "
+    }
+
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "DisplayPDF" {
+//            let controller: PDFViewerViewController = segue.destination as! PDFViewerViewController
+//            let pdfData = PDFConverter.convertView(view: view)
+//            controller.pdfData = pdfData
+        }
+    }
+
+    // MARK: - User prompt methods
+    
+    func promptForSendOption() {
+        let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        let sendAsEmailAction = UIAlertAction(title: "Send as Email", style: .default) {[weak self] (action) in
+            self!.sendImageInMail(image: self!.view.screenShot())
+            
+        }
+        let sendAsSMSAction = UIAlertAction(title: "Send as SMS", style: .default) {[weak self] (action) in
+            self!.sendImageInSMS(image: self!.view.screenShot())
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        actionSheet.addAction(sendAsEmailAction)
+        actionSheet.addAction(sendAsSMSAction)
+        actionSheet.addAction(cancelAction)
+        
+        present(actionSheet, animated: true, completion: nil)
+    }
+    
+    // MARK: - Utility
+    
+    func dateToString(now: Date) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM/dd/yyyy HH:mm:ss"
+        
+        return dateFormatter.string(from: now)
+    }
+
+    func displayAction(message: String) {
+        let alertController: UIAlertController = UIAlertController(title: "Mail Action", message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        
+        alertController.addAction(okAction)
+        present(alertController, animated: true, completion: nil)
+    }
+
+    // MARK: - Mail/SMS methods
+    
+    func sendImageInMail(image: UIImage) {
+        if (MFMailComposeViewController.canSendMail()) {
+            let mailComposer = MFMailComposeViewController()
+            mailComposer.mailComposeDelegate = self
+            
+            mailComposer.setSubject("Series as of \(dateToString(now: Date()))")
+            mailComposer.setMessageBody("My latest series as of \(dateToString(now: Date()))", isHTML: false)
+            mailComposer.addAttachmentData(image.pngData()!, mimeType: "image/png", fileName: "image.png")
+
+            present(mailComposer, animated: true, completion: nil)
+        } else {
+            displayAction(message: "Cannot send email.")
+        }
+    }
+    
+    func sendImageInSMS(image: UIImage) {
+        if (MFMessageComposeViewController.canSendAttachments()) {
+            let controller = MFMessageComposeViewController()
+            controller.messageComposeDelegate = self
+            
+            controller.body = "My latest series as of \(dateToString(now: Date()))"
+            controller.addAttachmentData(image.pngData()!, typeIdentifier: "image/png", filename: "image.png")
+            
+            present(controller, animated: true, completion: nil)
+        } else {
+            displayAction(message: "Cannot send SMS.")
+        }
+    }
+
+}
+
+// MARK: - Selector methods
+
+extension SeriesSummaryViewController {
+    @objc func send(_ sender: AnyObject) {
+        promptForSendOption()
     }
 }
+
+// MARK: - UICollectionViewDataSource
 
 extension SeriesSummaryViewController: UICollectionViewDataSource {
     func indexTitles(for collectionView: UICollectionView) -> [String]? {
@@ -173,6 +278,65 @@ extension SeriesSummaryViewController: UICollectionViewDataSource {
     
 }
 
+// MARK: - UICollectionViewDelegate
+
 extension SeriesSummaryViewController: UICollectionViewDelegate {
 
 }
+
+// MARK: - UIView utility
+
+extension UIView {
+    func screenShot() -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(bounds.size, isOpaque, UIScreen.main.scale)
+        let contextRef = UIGraphicsGetCurrentContext()
+        contextRef!.translateBy(x: -1.0, y: -1.0)
+        layer.render(in: contextRef!)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image!
+    }
+}
+
+// MARK: - MFMailComposeViewControllerDelegate
+
+extension SeriesSummaryViewController: MFMailComposeViewControllerDelegate {
+    
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        switch result {
+        case .cancelled:
+            print("Email cancelled")
+        case .saved:
+            print("Email saved")
+        case .sent:
+            print("Email sent")
+        case .failed:
+            print("Email failed")
+        default:
+            print("default")
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
+
+// MARK: - MFMessageComposeViewControllerDelegate
+
+extension SeriesSummaryViewController: MFMessageComposeViewControllerDelegate {
+    
+    func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
+        switch result {
+        case .cancelled:
+            print("SMS cancelled")
+        case .sent:
+            print("SMS sent")
+        case .failed:
+            print("SMS failed")
+        default:
+            print("default")
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
+
